bac59d1bb20b6ab8e14a53a3c5a84a3474620afd
Eter-Pag <274125838+Eter-Pag@users.noreply.github.com>
2026-04-07 02:44:15 -0400

    fix: actualizar URL del logo en STORE_CONFIG

--- shared/const.ts
+++ shared/const.ts
@@ -6,6 +6,6 @@ export const NOT_ADMIN_ERR_MSG = 'You do not have required permission (10002)';
 
 export const STORE_CONFIG = {
   storeName: "ETER KPOP MX",
-  logoUrl: "https://yt3.ggpht.com/rGD4n7Nq_CQfuIV0Ig_iIIbv30gVoKEcqcn1tsbWKZVWBA 9Ni1W5TVhp5vRMKplK0uBK7S4Ikw=s600-c-k-c0x00ffffff-no-rj-rp-mo",
+  logoUrl: "https://scontent.fntr7-1.fna.fbcdn.net/v/t39.30808-6/607586766_1948519082725497_6377161847596300243_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=1d70fc&_nc_eui2=AeFz8e3Jr0tb74xoir0bnyDI0jnM-Cuis0_SOcz4K6KzT4cv1RYis-bVgm4I7fO4IqJFBJ0imu1q9c1LcqkHH8Zm&_nc_ohc=nYugMUOtH4gQ7kNvwF5QBPu&_nc_oc=AdontMQvd_Uon47MQm7DP1SW3HxvLgvmWWRVXh0ZoCo4F0nyBnHI4dSMBKHAKkimao8&_nc_zt=23&_nc_ht=scontent.fntr7-1.fna&_nc_gid=_v_uoCDclHCF_hC7gC2hUQ&_nc_ss=7a3a8&oh=00_Af0dm8yICuIAFTubdvQ2lUKO1cD7uqKBE-V7GQhRCAw-Cg&oe=69DA6357",
   whatsappNumber: "526571921509",
 };
